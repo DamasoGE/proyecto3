33a5f61033ebb19231e1919de6244e57f3251cd4
Kuisyy <luis3valderrubio@gmail.com>
2024-05-08 14:27:38 +0200

    Esqueleto creado

--- src/programacion/piano/programa/ReproductorMidi.java
+++ src/programacion/piano/programa/ReproductorMidi.java
@@ -1,4 +1,39 @@
 package programacion.piano.programa;
 
-public class ReproductorMidi {
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.Receiver;
+import java.awt.*;
+
+public class ReproductorMidi implements Receiver {
+    private static final Color[] COLORES = {
+            Color.BLACK, Color.WHITE, Color.RED, Color.GREEN,
+            Color.BLUE, Color.YELLOW, Color.CYAN, Color.MAGENTA,
+            Color.GRAY, Color.PINK, Color.ORANGE, Color.LIGHT_GRAY,
+            Color.DARK_GRAY,
+            new Color(23, 217, 131),
+            new Color(180, 131, 78),
+            new Color(70, 102, 154)
+    };
+    private Piano piano;
+
+    public ReproductorMidi(){
+        throw new IllegalArgumentException();
+    }
+    public void reproducir(String ruta){
+        throw new IllegalArgumentException();
+    }
+    public void conectar(Piano p){
+        throw new IllegalArgumentException();
+    }
+
+    @Override
+    public void send(MidiMessage message, long timeStamp) {
+        throw new IllegalArgumentException();
+
+    }
+
+    @Override
+    public void close() {
+        throw new IllegalArgumentException();
+    }
 }
