7660c2fdef028fe1d013a12b701e6d80d5e62ff4
daniibueeno <danielbuenogarcia05@gmail.com>
2024-05-08 14:20:48 +0200

    Creo esqueleto

--- src/programacion/piano/teclas/ElementoVisual.java
+++ src/programacion/piano/teclas/ElementoVisual.java
@@ -1,4 +1,9 @@
 package programacion.piano.teclas;
 
+import java.awt.*;
+
 public interface ElementoVisual {
+    public void setPosicion(int x, int y);
+    public void setGraphics(Graphics g);
+    public void dibujar();
 }
