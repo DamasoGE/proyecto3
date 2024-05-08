f40e075a73f06ecd69eba0a8fb1ef59ba2efe6bc
frontalx <salahdinnasser13@gmail.com>
2024-05-08 14:29:14 +0200

    Piano hecho

--- src/programacion/piano/programa/Piano.java
+++ src/programacion/piano/programa/Piano.java
@@ -1,4 +1,39 @@
 package programacion.piano.programa;
 
-public class Piano {
+import programacion.piano.teclas.Tecla;
+
+import java.awt.*;
+
+public abstract class Piano {
+
+    private int teclaInicial;
+
+    private int teclaFinal;
+
+    protected Point posicion;
+
+    protected Graphics graphics;
+
+    public Piano(int ti, int tf){
+        this.teclaInicial=ti;
+        this.teclaFinal=tf;
+    }
+
+    public int getTeclaInicial(){
+        return this.teclaInicial;
+    }
+
+    public int getTeclaFinal(){
+        return this.teclaFinal;
+    }
+
+    public abstract Tecla getTecla(int canal, int nota);
+
+    public void setGraphics(Graphics graphics) {
+        this.graphics = graphics;
+    }
+
+    public void setPosicion(Point posicion) {
+        this.posicion = posicion;
+    }
 }
