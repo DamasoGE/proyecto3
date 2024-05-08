ae72444ef7fe21789b72c5feea93f704a80446e9
IsmaelVA <ivilchez.quesada@gmail.com>
2024-05-08 14:11:31 +0200

    Stub PianoSencillo

--- src/programacion/piano/programa/PianoSencillo.java
+++ src/programacion/piano/programa/PianoSencillo.java
@@ -1,4 +1,14 @@
 package programacion.piano.programa;
 
+import programacion.piano.teclas.Tecla;
+
+import java.util.Map;
+
 public class PianoSencillo {
+
+    private Map<Integer, Tecla> teclas;
+
+    public PianoSencillo(int teclaInicial, int teclaFinal){
+        throw new UnsupportedOperationException("Metodo sin programar");
+    }
 }
