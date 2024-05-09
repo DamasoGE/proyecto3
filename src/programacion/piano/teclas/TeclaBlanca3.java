4edc3edf643b8e56a825f406bc129afc428f785a
daniibueeno <danielbuenogarcia05@gmail.com>
2024-05-09 09:28:41 +0200

    Clase TeclaBlanca3 terminada

--- src/programacion/piano/teclas/TeclaBlanca3.java
+++ src/programacion/piano/teclas/TeclaBlanca3.java
@@ -7,11 +7,26 @@ public class TeclaBlanca3 extends TeclaBlanca{
 
     @Override
     protected int[] getVerticesX() {
-        return new int[0];
+        int[]verticesX = new int[6];
+        int N = TeclaNegra.ANCHURA, B = TeclaBlanca.ALTURA;
+        verticesX[0] = this.posicion.x+B-N/2;
+        verticesX[1] = this.posicion.x+B;
+        verticesX[2] = this.posicion.x+B;
+        verticesX[3] = this.posicion.x;
+        verticesX[4] = this.posicion.x;
+        verticesX[5] = this.posicion.x+B-N/2;
+        return verticesX;
     }
 
     @Override
     protected int[] getVerticesY() {
-        return new int[0];
+        int[]verticesY = new int[6];
+        verticesY[0] = this.posicion.y;
+        verticesY[1] = this.posicion.y;
+        verticesY[2] = this.posicion.y+TeclaBlanca.ALTURA;
+        verticesY[3] = this.posicion.y+TeclaBlanca.ALTURA;
+        verticesY[4] = this.posicion.y+TeclaNegra.ALTURA;
+        verticesY[5] = this.posicion.y+TeclaNegra.ALTURA;
+        return verticesY;
     }
 }
