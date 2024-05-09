079c32066af644f0bc60a48e51bd19a5f3f375fa
DamasoGE <franciscodamasogimenez@gmail.com>
2024-05-09 09:40:58 +0200

    Commit main Teclas factory

--- src/programacion/piano/teclas/TeclaFactory.java
+++ src/programacion/piano/teclas/TeclaFactory.java
@@ -5,6 +5,17 @@ public class TeclaFactory {
 
     }
     public static Tecla crearTecla(int nota){
-        throw new UnsupportedOperationException();
+        Tecla teclacreada;
+        int resto = nota/12;
+        if(resto==0 || resto==5){
+            teclacreada = new TeclaBlanca1(nota);
+        }else if(resto==2 || resto==7 || resto==9){
+            teclacreada = new TeclaBlanca2(nota);
+        }else if(resto==4 || resto==11){
+            teclacreada = new TeclaBlanca3(nota);
+        }else{
+            teclacreada = new TeclaNegra(nota);
+        }
+        return teclacreada;
     }
 }
