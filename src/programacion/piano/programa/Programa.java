144f439e19474f9de4fce617d0116e37c16835f6
DamasoGE <franciscodamasogimenez@gmail.com>
2024-05-10 08:35:38 +0200

    Commit main inicial viernes 10

--- src/programacion/piano/programa/Programa.java
+++ src/programacion/piano/programa/Programa.java
@@ -1,4 +1,21 @@
 package programacion.piano.programa;
 
+import bpc.daw.consola.Consola;
+import bpc.daw.consola.FondoColorSolido;
+
+import java.util.Scanner;
+
 public class Programa {
+    public static void main(String[] args){
+        Consola consola = new Consola();
+        consola.getCapaFondo().setFondo(new FondoColorSolido(0,0,70));
+        System.out.println("Introduce la ruta del archivo midi:");
+        String ruta = new Scanner(System.in).nextLine();
+        PianoSencillo pianoSencillo = new PianoSencillo(24,108);
+        pianoSencillo.setGraphics(consola.getCapaCanvas().getGraphics());
+        pianoSencillo.setPosicion(120,90);
+        ReproductorMidi reproductorMidi = new ReproductorMidi();
+        reproductorMidi.conectar(pianoSencillo);
+        reproductorMidi.reproducir(ruta);
+    }
 }
