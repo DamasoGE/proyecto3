bea182d633a33a42e7b53304972bf828bec9d5b1
Kuisyy <luis3valderrubio@gmail.com>
2024-05-13 09:33:25 +0200

    progrma semi arreglado

--- src/programacion/piano/programa/Programa.java
+++ src/programacion/piano/programa/Programa.java
@@ -1,19 +1,23 @@
 package programacion.piano.programa;
 
-import bpc.daw.consola.Consola;
-import bpc.daw.consola.FondoColorSolido;
+import bpc.daw.consola.*;
 
 import java.util.Scanner;
 
 public class Programa {
     public static void main(String[] args){
         Consola consola = new Consola();
+        CapaTexto ct = consola.getCapaTexto();
+        CapaCanvas cv = consola.getCapaCanvas();
+        Teclado teclado = consola.getTeclado();
         consola.getCapaFondo().setFondo(new FondoColorSolido(0,0,70));
-        System.out.println("Introduce la ruta del archivo midi:");
-        String ruta = new Scanner(System.in).nextLine();
+        ct.println("Introduce la ruta del archivo midi: ");
+        String ruta = teclado.leerCadenaCaracteres();
+
         PianoSencillo pianoSencillo = new PianoSencillo(24,108);
-        pianoSencillo.setGraphics(consola.getCapaCanvas().getGraphics());
+        pianoSencillo.setGraphics(cv.getGraphics());
         pianoSencillo.setPosicion(120,90);
+        pianoSencillo.dibujar();
         ReproductorMidi reproductorMidi = new ReproductorMidi();
         reproductorMidi.conectar(pianoSencillo);
         reproductorMidi.reproducir(ruta);
