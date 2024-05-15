c8833e8d8e90c75bb9a5b9f83d93740e3ef7054e
IsmaelVA <ivilchez.quesada@gmail.com>
2024-05-15 14:18:06 +0200

    Empezando clase MultiPiano

---
+++ src/programacion/piano/programa/MultiPiano.java
@@ -0,0 +1,53 @@
+package programacion.piano.programa;
+
+import programacion.piano.teclas.Tecla;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiPiano extends Piano{
+
+    List<PianoSencillo> pianos;
+
+    public MultiPiano(int teclaInicial, int teclaFinal) {
+        super(teclaInicial, teclaFinal);
+        pianos = new ArrayList<>();
+
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+        pianos.add(new PianoSencillo(teclaInicial, teclaFinal));
+    }
+
+    @Override
+    public Tecla getTecla(int canal, int nota) {
+        return null;
+    }
+
+    @Override
+    public int getAnchura() {
+        return 0;
+    }
+
+    @Override
+    public int getAltura() {
+        return 0;
+    }
+
+    @Override
+    public void dibujar() {
+
+    }
+}
